71cbc21762f39b257105402f64f0548efbc93c48
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-06 20:55:14 +0100

    add config variables (non private)

---
+++ frontend/config.js
@@ -0,0 +1,6 @@
+export const wss = `wss://rideout-prod-7ab6085489.herokuapp.com/rideout-db/dev`;
+export const endpoint = `https://rideout-prod-7ab6085489.herokuapp.com/rideout-db/dev`;
+// export const wss = `ws://localhost:4000`;
+// export const endpoint = `http://localhost:4000`;
+export const PROXY = 'https://cors-anywhere.herokuapp.com/';
+export const EVENTS_PER_PAGE = 100000;
