b5f7aefe1cf7d5aa5badefd361a12fb99738e0f6
Ross OBrien <ross@Rosss-MBP.home>
2019-08-11 11:22:33 +0100

    cors

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -64,7 +64,7 @@ const Mutation = {
       {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 365,
-        domain: 'rideout.rocks',
+        domain: 'www.rideout.rocks',
       },
       info
     );
