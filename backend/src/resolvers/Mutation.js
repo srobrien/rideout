ac0afcb81de26bd4c0643963e48ae04263718b59
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-15 18:08:11 +0100

    cors

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -64,7 +64,7 @@ const Mutation = {
       {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 * 365,
-        domain: 'rideout.rocks',
+        domain: 'rossobrien.tech',
         sameSite: 'lax',
       },
       info
