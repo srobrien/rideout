5ef7c27cb1ecd77dd73675dadd5575492ee316c5
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-15 18:05:28 +0100

    cors

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -22,7 +22,7 @@ function createClient({ headers }) {
       fetchOptions: {
         credentials: 'include',
       },
-      headers,
+      headers: { cookie: headers && headers.cookie },
     });
   };
 
