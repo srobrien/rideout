2fb17401224d659f14de12a1a3c021ac1e74ebe9
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-18 15:28:15 +0100

    added comments

--- frontend/components/context/Auth.js
+++ frontend/components/context/Auth.js
@@ -6,10 +6,10 @@ import { CURRENT_USER_QUERY } from '../../graphql/Query';
 export const AuthContext = createContext();
 
 const Auth = ({ children }) => {
-  const { data } = useQuery(CURRENT_USER_QUERY);
+  const { data } = useQuery(CURRENT_USER_QUERY); // gets currently logged in user information from the DB.
   if (data) {
     return (
-      <AuthContext.Provider value={data.user}>{children}</AuthContext.Provider>
+      <AuthContext.Provider value={data.user}>{children}</AuthContext.Provider> // provides the user as context which can be accessed throughout the application when required.
     );
   }
   return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
