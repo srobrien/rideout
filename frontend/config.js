83ab5036d38c9dd0b14fb57f7518a31ba1d77a2f
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-19 19:16:47 +0100

    changed endpoint for backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-// export const endpoint = `https://www.rossobrien.tech/`;
-export const endpoint = `http://localhost:4000`;
+export const endpoint = `https://www.rossobrien.tech/`;
+// export const endpoint = `http://localhost:4000`;
 export const PROXY = 'https://cors-anywhere.herokuapp.com/';
