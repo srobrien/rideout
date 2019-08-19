0cab85a1772b8411a9178c76abdd7c255f7d33d9
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-19 20:36:32 +0100

    added no event found message

--- frontend/components/EventListing.js
+++ frontend/components/EventListing.js
@@ -4,13 +4,26 @@ import EventItem from './EventItem';
 import { EventContainer } from './styled/StyledEventsHomePage';
 
 // renders a list of event item cards on the events page.
-const EventListing = ({ events }) => (
-  <EventContainer>
-    {events.map(e => (
-      <EventItem key={e.id} event={e} />
-    ))}
-  </EventContainer>
-);
+const EventListing = ({ events }) => {
+  if (events.length === 0) {
+    return (
+      <EventContainer>
+        <div
+          style={{ height: '100vh', textAlign: 'center', paddingTop: '20px' }}
+        >
+          <h3 style={{ color: '#fff' }}>Sorry! No events found!</h3>
+        </div>
+      </EventContainer>
+    );
+  }
+  return (
+    <EventContainer>
+      {events.map(e => (
+        <EventItem key={e.id} event={e} />
+      ))}
+    </EventContainer>
+  );
+};
 
 export default EventListing;
 
