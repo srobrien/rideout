865a6d40449782807f866f54819af0db7abe112d
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-20 18:44:41 +0100

    config

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-// export const endpoint = `https://rideout-app-backend.herokuapp.com/`;
-export const endpoint = `http://localhost:4000`;
+export const endpoint = `https://rideout-app-backend.herokuapp.com/`;
+// export const endpoint = `http://localhost:4000`;
 export const PROXY = 'https://cors-anywhere.herokuapp.com/';
