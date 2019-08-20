2de574816bd61a1ff8131edeffb45bac6dac1501
Ross OBrien <ross@Rosss-MacBook-Pro.local>
2019-08-20 19:15:26 +0100

    addoverflow

--- frontend/components/styled/StyledEvent.js
+++ frontend/components/styled/StyledEvent.js
@@ -65,6 +65,7 @@ export const Card = styled.div`
   box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
   padding: 20px 5px 5px 5px;
   max-height: ${props => props.height || null};
+  overflow-y: scroll;
 `;
 
 export const DetailSet = styled.div`
